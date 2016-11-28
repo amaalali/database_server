7d88f5bb067098aff272b24adc90140e1379dcf2
Amaal Ali <amq.ali@gmail.com>
2016-11-28 15:46:28 +0000

    Test for retreiving data.

--- spec/databaseserver_feature_spec.rb
+++ spec/databaseserver_feature_spec.rb
@@ -13,7 +13,13 @@ feature 'DatabaseServer' do
       visit 'http://localhost:4000/set?somekey=somevalue'
       expect(page.status_code).to be(200)
     end
+  end
 
-    
+  feature 'Output' do
+    scenario 'value can be retreived' do
+      visit 'http://localhost:4000/set?somekey=somevalue'
+      visit 'http://localhost:4000/get?key=somekey'
+      expect(page).to have_content('somevalue')
+    end
   end
 end
