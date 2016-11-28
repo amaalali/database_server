35cbfca8164318851e717bd9a9673ca8f2f1b600
Amaal Ali <amq.ali@gmail.com>
2016-11-28 15:11:29 +0000

    Test for route /set to exist. -/-

--- spec/databaseserver_feature_spec.rb
+++ spec/databaseserver_feature_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 
 feature 'DatabaseServer' do
-  feature 'on custom port 400' do
-    scenario 'status is 200' do
+  feature 'Setup' do
+    scenario 'on custom port 400, status is 200' do
       visit 'http://localhost:4000'
+      expect(page.status_code).to be(200)
+    end
+  end
+
+  feature 'Input' do
+    scenario '' do
+      visit 'http://localhost:4000/set?somekey=somevalue'
       expect(page.status_code).to be(200)
     end
   end
